a3f4a41d36b4d7cee14da86e35740b86152dad16
defg43 <nino.obradovic22@gmail.com>
2023-08-28 19:15:58 +0200

    updated pattern matching syntax

--- src/matching.h
+++ src/matching.h
@@ -76,31 +76,46 @@
 #define _matchany_foreach_ladder64(F, a, n, ...) F(_64, n) __VA_OPT__(_matchany_foreach_ladder65(F, a, __VA_ARGS__))
 #define _matchany_foreach_ladder65(F, a, n, ...) _Pragma("GCC error \"exceeded 64 arguments, add more\"");
 
-typedef struct { int placeholder; } _matcher_hidden_wildcard_type;
-
-#define wildcard(expr) _matcher_hidden_wildcard_type expr = {0}; if(0) { }
+typedef struct { bool iterator; bool matched; } _matcher_hidden_wildcard_type;
 
 #define _match_struct_element_numbered(mod, prop) typeof(prop) mod;
 
 #define _matchany_generate_elements(...) \
 	_matchany_foreach(_match_struct_element_numbered,,__VA_ARGS__)
 	
-#define match(...) for( struct { bool matched; \
-	_matchany_generate_elements(__VA_ARGS__) } \
-    _local_matching_object = { false,  __VA_ARGS__}; \
-    ({static int _match_iter = 0; \
-    _match_iter ^= 1 ;_match_iter == 1;}); /*nothing*/)
+#define match(...) for( _matcher_hidden_wildcard_type _ = { 0, 0 }; _.iterator == false ; _.iterator = true) \
+	for( struct { _matchany_generate_elements(__VA_ARGS__) } \
+    _local_matching_object = {__VA_ARGS__}; \
+    _.iterator == false; _.iterator = true)
 
+#define pattern(...) \
+	_Pragma("GCC diagnostic push") \
+	_Pragma("GCC diagnostic ignored \"-Wint-conversion\"") \
+	if( ({ if(_.matched == true) break; \
+	_.matched = (_create_comparisons(__VA_ARGS__)); \
+	_.matched; \
+	}) )\
+	_Pragma("GCC diagnostic push")
 
-#define pattern(...) if(_local_matching_object.matched = \
-	!_local_matching_object.matched && _create_comparisons(__VA_ARGS__))
+#define _comparison_or_defaulter_old(arg1, arg2) \
+	_Generic((arg2), \
+		_matcher_hidden_wildcard_type: true, \
+		default: compare(_local_matching_object.arg1, \
+		(__builtin_types_compatible_p(typeof(arg2), \
+		typeof(_local_matching_object.arg1))) \
+		? arg2 : _local_matching_object.arg1) \
+	)
 
-#define _comparison_or_defaulter(arg1, arg2) _Generic((arg2), \
-	_matcher_hidden_wildcard_type: true, \
-	default: compare(_local_matching_object.arg1, arg2))
+#define _comparison_or_defaulter(arg1, arg2) \
+	_Generic((arg2), \
+		_matcher_hidden_wildcard_type: true, \
+		default: \
+		compare(_local_matching_object.arg1, arg2) \
+	)	
 
-#define _create_comparisons(...) true _matchany_foreach(&& \
-	_comparison_or_defaulter,,__VA_ARGS__)
+#define _create_comparisons(...) \
+	true _matchany_foreach(&& \
+	_comparison_or_defaulter,,__VA_ARGS__)	
 	
 #define when ) && (
 
