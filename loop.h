edcebe897eb0b77c26e0487ed1240b0d2130cb6f
defg43 <nino.obradovic22@gmail.com>
2023-10-07 14:51:38 +0200

    added maybe

--- loop.h
+++ loop.h
@@ -5,8 +5,13 @@
 #ifndef LOOP_H
 #define LOOP_H
 
+#include <stdio.h>
+#include <time.h>
+
 #define loop for(;;)
-#define repeat for(;;)
+#define repeat(num) for(int _hidden_iteartor_i_dont_touch = 0; i < num; num++)
+
+#define maybe(num) if(({srand(time(NULL); rand() > num * RAND_MAX);}))
 
 #define defer(pre, post) for(pre; ({static int _hidden_iterator_d_dont_touch = 2; --_hidden_iterator_d_dont_touch;}); post)
 
