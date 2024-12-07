c078827f289f88f44cc1b54425f4a908b5d956be
defg43 <nino.obradovic22@gmail.com>
2024-12-07 02:41:12 +0100

    cursed iteration capabilities

--- foreach.h
+++ foreach.h
@@ -8,6 +8,7 @@
 #define FOREACH_H
 
 #include <assert.h>
+#include "types.h"
 
 #define sized ,,,
 #define of ,,
@@ -15,8 +16,20 @@
 
 #define where ) if(
 
-#define lengthof(x) ((sizeof(x)/sizeof(0[x])) /\
-	((size_t)(!(sizeof(x) % sizeof(0[x])))))
+#define arrayAsPtr(arr) ({												\
+	struct {															\
+		typeof(decay(arr)) element;										\
+		size_t count;													\
+	} dumy;																\
+	__builtin_choose_expr(is_struct(arr), arr, dumy.element) = arr;		\
+	__builtin_choose_expr(is_struct(arr), arr, dumy).element;			\
+})
+
+#define lengthof(arr) ({																		\
+	struct { size_t count; } dumy = { .count = sizeof arr / sizeof arrayAsPtr(arr)[0] };		\
+	__builtin_choose_expr(is_struct(arr), arr, dumy).count;										\
+})
+										
 
 #ifdef __cplusplus
 #include <type_traits>
@@ -24,7 +37,9 @@
 #define assert_not_array(a, msg) static_assert(!std::is_array_v<typeof(a)>, msg)	
 #else
 #define assert_array(a, msg) \
-    static_assert(1 - __builtin_types_compatible_p(typeof(a), typeof(&(a)[0])), msg)
+    static_assert(is_array(a), msg)
+#define assert_iterable(a, msg) \
+	static_assert(is_array(a) + is_struct(a), msg)
 #define assert_not_array(a, msg) \
 	static_assert(__builtin_types_compatible_p(typeof(a), typeof(&(a)[0])), msg)
 #endif // __cplusplus
@@ -34,8 +49,8 @@
 #define _foreach_from_function(result, execution, condition) \
 	for( struct { typeof(result) result_copy; size_t _iterator; } = { ._iterator = 0 };; }
 
-#define _foreach_index(index, arr) \
-assert_array(arr, "argument is not array, declare size with: <n> sized <pointer>");\
+#define _foreach_index(index, arr) 																															\
+assert_iterable(arr, "argument is not iterable, declare size with: <n> sized <pointer>");																			\
 for(index, _hidden_foreach_index_dont_use = 0, _hidden_foreach_toggler = 1; _hidden_foreach_index_dont_use < lengthof(arr); _hidden_foreach_index_dont_use) \
 for(index = _hidden_foreach_index_dont_use; _hidden_foreach_toggler = _hidden_foreach_toggler ^ 1; _hidden_foreach_index_dont_use++)
 
@@ -45,19 +60,19 @@ for(index, _hidden_foreach_index_dont_use = 0, _hidden_foreach_toggler = 1; _hid
 for(index = _hidden_foreach_index_dont_use; _hidden_foreach_toggler = _hidden_foreach_toggler ^ 1; _hidden_foreach_index_dont_use++)
 
 #define _foreach_element(element, _1, arr) \
-assert_array(arr, "argument is not array, declare size with: <n> sized <pointer>");\
+assert_iterable(arr, "argument is not iterable, declare size with: <n> sized <pointer>");\
 for(size_t _iterator = 0, _hidden_foreach_toggler = 1; _iterator < lengthof(arr);) \
-for(element = arr[_iterator]; _hidden_foreach_toggler = _hidden_foreach_toggler ^ 1; _iterator++)
+for(element = arrayAsPtr(arr)[_iterator]; _hidden_foreach_toggler = _hidden_foreach_toggler ^ 1; _iterator++)
 
 #define _foreach_element_sized(element, _1, size, _2, _3, arr) \
 assert_not_array(arr, "size can only be specified for non-arrays");\
 for(size_t _iterator = 0, _hidden_foreach_toggler = 1; _iterator < size;) \
-for(element = arr[_iterator]; _hidden_foreach_toggler = _hidden_foreach_toggler ^ 1; _iterator++)
+for(element = arrayAsPtr(arr)[_iterator]; _hidden_foreach_toggler = _hidden_foreach_toggler ^ 1; _iterator++)
 
 #define _foreach_single(arr) \
 for(struct {int index; typeof(0[arr]) element;}iter = \
 	{ .index = 0, .element = 0[arr] }; \
-	iter.index < lengthof(arr); iter.element = arr[++iter.index] )
+	iter.index < lengthof(arr); iter.element = arrayAsPtr(arr)[++iter.index] )
 
 #define _foreach_type(_1, _2, _3, _4, _5, _6, _7, foreachnum, ...) foreachnum
 #define _foreach(...) _foreach_type(__VA_ARGS__, _foreach_err, \
